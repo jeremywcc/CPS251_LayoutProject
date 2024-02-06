bc5aceeb754973ec65efb00c88c2ff77786fb886
JeremyWCC <jesutton@wccnet.edu>
2024-02-06 00:35:29 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cps251_layoutproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cps251_layoutproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
